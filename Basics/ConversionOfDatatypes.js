502afeca7eb56b8da68261fd699bf90a869ddac2
Shiwi-1234 <122026730+Shiwi-1234@users.noreply.github.com>
2024-01-02 10:58:26 +0000

    conversion

---
+++ Basics/ConversionOfDatatypes.js
@@ -0,0 +1,15 @@
+let num = 33
+console.log(typeof num)
+
+//this will change the data type of num variable
+let changeNum = String (num)  
+console.log(changeNum) 
+console.log(typeof changeNum) 
+
+//
+let login = "dds"
+console.log(login) 
+console.log(typeof login)
+let changeLogin = Boolean(login)
+console.log(typeof changeLogin)
+
